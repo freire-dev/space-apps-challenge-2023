2d9d5d5234a0480f6f6d51f9051ec37146da18e8
bmsrangel <bmsrangel@hotmail.com>
2023-10-08 12:23:16 -0300

    Created RecommendationCard component

---
+++ pages/components/RecommendationCard/RecommendationCard.tsx
@@ -0,0 +1,29 @@
+import styles from "./RecommendationCard.module.css";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+
+type RecommendationCardProps = {
+  icon: string;
+  category: string;
+  recommendation: string;
+};
+
+export function RecommendationCard(props: RecommendationCardProps) {
+  return (
+    <div className={`${inter.className} & ${styles.card}`}>
+      <div className={styles.cardHeader}>
+        <Image
+          alt={props.icon}
+          src={props.icon}
+          width={34}
+          height={34}
+          className={styles.cardImage}
+          objectFit="height"
+        />
+        <text>{props.category}</text>
+      </div>
+      <text className={styles.cardContent}>{props.recommendation}</text>
+    </div>
+  );
+}
